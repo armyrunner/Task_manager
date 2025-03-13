e9e79f7302eb8d90616b963022e6d7c948b8a2f6
Andrew <andrew.c.nelson@tcnbroadcasting.com>
2025-03-13 15:26:53 -0600

    Moved files into new directories. Python and Golang.

--- task_manager_python/pdf_generator.py
+++ task_manager_python/pdf_generator.py
@@ -2,19 +2,17 @@ from reportlab.lib.pagesizes import letter,landscape
 from reportlab.pdfgen import canvas
 from reportlab.platypus import SimpleDocTemplate,Table,TableStyle
 from reportlab.lib import colors
-from task_manager import Task_Manager
 
 class PDF_Generator():
 
-    def __init__(self,filename):
+    def __init__(self,tasks,filename,task_title):
         self.filename = filename
-        self.tasks = task_manager.Task_manager()
+        self.tasks = tasks
         self.canvas = canvas.Canvas(filename,pagesize=landscape(letter))
         self.width, self.height = landscape(letter)
         
         #set variables for the different function calls
-        self.curr_task_title = 'Current Task List'
-        self.comp_task_title = 'Completed Task List'
+        self.curr_task_title = task_title
         self.fontName_title = 'Times-Roman'
         self.col_fontName = 'Courier-bold'
         self.row_fontName = 'Courier'
@@ -39,40 +37,32 @@ class PDF_Generator():
                 filename,
                 pagesizes=landscape(letter)
                 )
-        cur_task_table = Table() #TODO need data for the table
+        task_table = Table(self.tasks) #Getting Data from Task_Manager
 
         elems = []
-        elems.append(cur_task_table) # puts all data onto the document
+        elems.append(task_table) # puts all data onto the document
         
-        style = TableStyle([
+        # adding alternating colors for the table for fancier table look.
+        # adding boarder to the table as well.
+        rowNumb = len(task_table) #Data from Task Manager to determine how many rows
+        for i in range(1,rowNumb):
+            if i % 2 == 0:
+                bc = colors.burlywood
+            else:
+                bc = colors.beige
+
+        styleConfig = TableStyle([
             ('BACKGROUND',(0,0),(3,0),colors.green),
             ('TEXTCOLOR',(0,0),(-1,0),colors.whitesmoke), # the -1 is the end of the row no matter how long the row is
             ('ALIGN',(0,0),(-1,-1),'CENTER') # (-1,-1) basically the whole table will be center aligned
             ('FONTNAME', (0,0),(-1,0),' Courier-bold),
             ('FONTSIZE',(0,0),(-1,0),14),
             ('BOTTOMPADDING',(0,0),(-1,0),12),
-            ('BACKGROUND',(0,1),(-1,-1),colors.beige),
-
-            ])
-        table.setStyle(style) # set table with solid color for all table, with color for column names
-        
-        # adding alternating colors for the table for fancier table look.
-        # adding boarder to the table as well.
-        rowNumb = len() # TODO Need data to get the length of rows
-        for i in range(1,rowNumb):
-            if i % 2 == 0:
-                bc = colors.burlywood
-            else:
-                bc = colors.beige
-        
-        
-        alt_color_table = TableStyle([
             ('BACKGROUND',(0,1),(-1,-1),bc),
-            ('BOX',(0,0),(-1,-1),2,colors.black),
             ('GRID',(0,1),(-1,-1),2,colors.black),
             ])
-        table.setStyle(alt_color_table)
-
+        table.setStyle(styleConfig) # set table with solid color for all table, with color for column names
+        
         c.save()
 
 
