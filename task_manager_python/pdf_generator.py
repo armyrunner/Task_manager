ff95b776105859afde55f64a65dea570bc1709da
armyrunner <live2runmarathon@gmail.com>
2025-03-20 20:56:29 -0600

    made small changes

--- task_manager_python/pdf_generator.py
+++ task_manager_python/pdf_generator.py
@@ -120,55 +120,47 @@ def create_task_table(self, section_title, tasks):
 Let me know if you need further adjustments!
 
 '''
-
-  
-    
     def create_pdf(self): 
         
- 
         current_tasks = [task for task in self.tasks if task.status != "Completed" or task.status != 'completed']
         completed_tasks = [task for task in self.tasks if task.status == "Completed" or task.status == 'completed']
         
-
-        if current_tasks:
-            self.canvas = canvas.Canvas('cur_task_report.pdf', pagesize=letter)
-            self.task_title = 'Current Task List'
                  
-            #find the middle of the page to center the title of the report
-            text_width = self.canvas.stringWidth(self.task_title,self.fontName_title,self.fontSize_title)
-            x_pos = (self.width - text_width) / 2
-            y_pos = self.height - 40
+        #find the middle of the page to center the title of the report
+        text_width = self.canvas.stringWidth(self.task_title,self.fontName_title,self.fontSize_title)
+        x_pos = (self.width - text_width) / 2
+        y_pos = self.height - 40
 
-            #Drawing the string on the document to see Title of Document
-            self.canvas.setFont(self.fontName_title,self.fontSize_title)
-            self.canvas.drawString(x_pos,y_pos,self.task_title)
+        #Drawing the string on the document to see Title of Document
+        self.canvas.setFont(self.fontName_title,self.fontSize_title)
+        self.canvas.drawString(x_pos,y_pos,self.task_title)
 
-            #creating the table for the document
-            pdf_doc = SimpleDocTemplate(self.filename,pagesizes=landscape(letter))
-       
-            # adding alternating colors for the table for fancier table look.
-            # adding boarder to the table as well.
-            rowNumb = len(current_tasks) #Data from Task Manager to determine how many rows
-            for i in range(1,rowNumb):
-                if i % 2 == 0:
-                    bc = colors.burlywood
-                else:
-                    bc = colors.beige
+        #creating the table for the document
+        pdf_doc = SimpleDocTemplate(self.filename,pagesizes=landscape(letter))
+    
+        # adding alternating colors for the table for fancier table look.
+        # adding boarder to the table as well.
+        rowNumb = len(current_tasks) #Data from Task Manager to determine how many rows
+        for i in range(1,rowNumb):
+            if i % 2 == 0:
+                bc = colors.burlywood
+            else:
+                bc = colors.beige
 
-            styleConfig = TableStyle([
-                ('BACKGROUND',(0,0),(3,0),colors.green),
-                ('TEXTCOLOR',(0,0),(-1,0),colors.whitesmoke), # the -1 is the end of the row no matter how long the row is
-                ('ALIGN',(0,0),(-1,-1),'CENTER'), # (-1,-1) basically the whole table will be center aligned
-                ('FONTNAME', (0,0),(-1,0),self.col_fontName),
-                ('FONTNAME',(0,1),(-1,-1),self.row_fontName),
-                ('FONTSIZE',(0,0),(-1,0),14),
-                ('BOTTOMPADDING',(0,0),(-1,0),12),
-                ('BACKGROUND',(0,1),(-1,-1),bc),
-                ('GRID',(0,1),(-1,-1),2,colors.black),
-                ])
-            current_tasks.setStyle(styleConfig) # set table with solid color for all table, with color for column names
-            
-            pdf_doc.build([current_tasks]])
-        self.canvas.save()
+        styleConfig = TableStyle([
+            ('BACKGROUND',(0,0),(3,0),colors.green),
+            ('TEXTCOLOR',(0,0),(-1,0),colors.whitesmoke), # the -1 is the end of the row no matter how long the row is
+            ('ALIGN',(0,0),(-1,-1),'CENTER'), # (-1,-1) basically the whole table will be center aligned
+            ('FONTNAME', (0,0),(-1,0),self.col_fontName),
+            ('FONTNAME',(0,1),(-1,-1),self.row_fontName),
+            ('FONTSIZE',(0,0),(-1,0),14),
+            ('BOTTOMPADDING',(0,0),(-1,0),12),
+            ('BACKGROUND',(0,1),(-1,-1),bc),
+            ('GRID',(0,1),(-1,-1),2,colors.black),
+            ])
+        current_tasks.setStyle(styleConfig) # set table with solid color for all table, with color for column names
+        
+        pdf_doc.build([current_tasks]])
+
 
 
